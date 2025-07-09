5bea923b8513df6616ec4922d8f819729a2f0bdd
jbelew <john.belew@gmail.com>
2025-07-09 12:29:55 -0700

    Removed unneeded suspense hook.

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,8 @@ import "@radix-ui/themes/tokens/colors/cyan.css";
 import "@radix-ui/themes/tokens/colors/sage.css";
 import "@radix-ui/themes/tokens/colors/purple.css";
 
-
 import "@radix-ui/themes/components.css";
 import "@radix-ui/themes/utilities.css";
-
 
 // Main App CSS
 import "./index.css";
@@ -17,32 +15,19 @@ import "./index.css";
 import "./i18n/i18n"; // Initialize i18next
 
 import { Theme } from "@radix-ui/themes";
-import { StrictMode, Suspense } from "react"; // Added Suspense
+import { StrictMode } from "react"; // Added Suspense
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundry/ErrorBoundry";
-import MessageSpinner from "./components/MessageSpinner/MessageSpinner";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<ErrorBoundary>
 				<Theme appearance="dark" panelBackground="translucent" accentColor="cyan" grayColor="sage">
-					<Suspense
-						fallback={
-							<main className="flex flex-col items-center justify-center lg:min-h-screen">
-								<MessageSpinner
-									isVisible={true}
-									isInset={true}
-									initialMessage="Activating Uplink..."
-								/>
-							</main>
-						}
-					>
-						<App />
-					</Suspense>
+					<App />
 				</Theme>
 			</ErrorBoundary>
 		</BrowserRouter>
