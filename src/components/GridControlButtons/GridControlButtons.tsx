35fd640b1b05c08ef46f57de29d6b39ec7062516
jbelew <john.belew@gmail.com>
2025-07-16 19:06:54 -0700

    One more, small, visual change.

--- src/components/GridControlButtons/GridControlButtons.tsx
+++ src/components/GridControlButtons/GridControlButtons.tsx
@@ -51,7 +51,8 @@ const GridControlButtons: React.FC<RowControlButtonProps> = ({
 				<Tooltip content={t("gridControls.activateRow")}>
 					<IconButton
 						size={iconButtonSize}
-						variant="surface"
+						variant="outline"
+						radius="full"
 						className={`shadow-sm ${!hasModulesInGrid ? "!cursor-pointer" : ""}`} // Centering handled by parent
 						onClick={() => activateRow(rowIndex)}
 						disabled={hasModulesInGrid || gridFixed}
@@ -65,8 +66,9 @@ const GridControlButtons: React.FC<RowControlButtonProps> = ({
 			{isLastActiveRow && (
 				<Tooltip content={t("gridControls.deactivateRow")}>
 					<IconButton
-						variant="surface"
+						variant="outline"
 						size={iconButtonSize}
+						radius="full"
 						className={` ${!hasModulesInGrid ? "!cursor-pointer" : ""}`} // Centering handled by parent
 						onClick={() => deActivateRow(rowIndex)}
 						disabled={hasModulesInGrid || gridFixed}
