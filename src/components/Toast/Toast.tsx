23452a6524afb0099785d177a55d1ed95cd0446a
John Belew <john.belew@gmail.com>
2025-10-23 07:58:30 -0700

    refactor(toast): increased display duration and added close on click functionality

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -11,7 +11,14 @@ type ToastProps = {
 
 export const NmsToast = ({ open, onOpenChange, title, description }: ToastProps) => {
 	return (
-		<Toast.Root duration={10000} className="ToastRoot" open={open} onOpenChange={onOpenChange}>
+		<Toast.Root
+			duration={10000}
+			className="ToastRoot"
+			open={open}
+			onOpenChange={onOpenChange}
+			onClick={() => onOpenChange(false)}
+			onTouchEnd={() => onOpenChange(false)}
+		>
 			<Toast.Title className="ToastTitle">{title}</Toast.Title>
 			<Toast.Description className="ToastDescription">{description}</Toast.Description>
 		</Toast.Root>
