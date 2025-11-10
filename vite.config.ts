1d38b92fc63b8197e661fd9f313826480273d90a
John Belew <john.belew@gmail.com>
2025-11-09 19:36:13 -0800

    chore(caching): aligned maifest and middleware cache values

--- vite.config.ts
+++ vite.config.ts
@@ -90,7 +90,18 @@ export default defineConfig(({ mode }) => {
 								cacheName: "images-cache",
 								expiration: {
 									maxEntries: 100,
-									maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+									maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+								},
+							},
+						},
+						{
+							urlPattern: ({ request }) => request.destination === "font",
+							handler: "CacheFirst",
+							options: {
+								cacheName: "fonts-cache",
+								expiration: {
+									maxEntries: 30,
+									maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
 								},
 							},
 						},
@@ -102,7 +113,7 @@ export default defineConfig(({ mode }) => {
 								networkTimeoutSeconds: 5,
 								expiration: {
 									maxEntries: 50,
-									maxAgeSeconds: 60 * 60 * 24 * 7, // 1 week
+									maxAgeSeconds: 60 * 60 * 24, // 1 day
 								},
 								cacheableResponse: {
 									statuses: [0, 200],
@@ -112,11 +123,12 @@ export default defineConfig(({ mode }) => {
 						{
 							urlPattern: ({ request }) =>
 								request.destination === "script" || request.destination === "style",
-							handler: "StaleWhileRevalidate",
+							handler: "CacheFirst",
 							options: {
 								cacheName: "static-resources",
 								expiration: {
 									maxEntries: 50,
+									maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
 								},
 							},
 						},
