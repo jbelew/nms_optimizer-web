1b830c6a0a78a2a0c8fcafb2d5299a701aad26ae
John Belew <john.belew@gmail.com>
2025-11-22 02:56:12 -0800

    chore(csp): allow YouTube embeds

--- server/app.js
+++ server/app.js
@@ -40,6 +40,7 @@ const csp = [
 	"img-src 'self' data: www.google-analytics.com",
 	"font-src 'self'",
 	"connect-src 'self' https://nms-optimizer-service-afebcfd47e2a.herokuapp.com wss://nms-optimizer-service-afebcfd47e2a.herokuapp.com https://*.google-analytics.com https://*.googletagmanager.com https://api.nms-optimizer.app",
+	"frame-src https://www.youtube.com",
 	"frame-ancestors 'none'",
 	"object-src 'none'",
 	"base-uri 'self'",
