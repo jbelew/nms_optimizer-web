1637072f4bf5af05ce57382e5d2f0e7b3f5aae7a
John Belew <john.belew@gmail.com>
2026-04-07 19:45:15 -0700

    ci: configure lighthouse to emulate mobile device settings for performance audits

--- scripts/lighthouserc.cjs
+++ scripts/lighthouserc.cjs
@@ -3,6 +3,24 @@ module.exports = {
     collect: {
       url: ['http://127.0.0.1:4173/'],
       startServerCommand: 'npm run preview -- --host 127.0.0.1',
+      settings: {
+        formFactor: 'mobile',
+        throttlingMethod: 'devtools',
+        throttling: {
+          cpuSlowdownMultiplier: 1.5,
+          downloadThroughputKbps: 10240, // 10 Mbps
+          uploadThroughputKbps: 5120, // 5 Mbps
+          requestLatencyMs: 40,
+        },
+        screenEmulation: {
+          mobile: true,
+          width: 393,
+          height: 852,
+          deviceScaleFactor: 3,
+          disabled: false,
+        },
+        emulatedUserAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1',
+      }
     },
     upload: {
       target: 'temporary-public-storage',
