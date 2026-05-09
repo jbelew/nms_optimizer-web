e25f02e42e2dc20bf9b53fc9bbbbb4a4eb5f68fa
John Belew <john.belew@gmail.com>
2026-05-09 08:53:02 -0700

    fix(tests): resolve Location type regression in useLanguage.test.tsx

--- src/hooks/useLanguage/useLanguage.test.tsx
+++ src/hooks/useLanguage/useLanguage.test.tsx
@@ -17,7 +17,6 @@ const createMockLocation = (pathname: string): Location => ({
 	hash: "",
 	state: null,
 	key: "default",
-	unstable_mask: undefined,
 });
 
 // No need to declare useLocationMock at top level anymore
